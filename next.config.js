64d038d4937d812b890efd83f1ec6c8689c39c3e
Tech4Loop Developer <dev@tech4loop.com>
2025-11-30 11:39:39 -0400

    fix: Add github.com to allowed image domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'avatars.githubusercontent.com'],
+    domains: ['images.unsplash.com', 'avatars.githubusercontent.com', 'github.com'],
     formats: ['image/avif', 'image/webp'],
   },
 }
